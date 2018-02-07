5b1194d5834a16b2ff5f94e8b39e3bd6e35beccb
Unknown <facig16@gmail.com>
2018-02-07 21:04:44 +0100

    Modifications scripts évenements

---
+++ Assets/Scripts/Events/Evenement.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Evenement  {
+    // Variables propres à l'événement
+    private int id; // Son id pour retrouver
+	private static int nbEvenement=2; // Le nombre d'événement (instance?) crée en tout
+    public float proba; // Proba de tomber sur l'événement
+    public String conditions; // Alors ça ??
+    
+    // Variables textuelles
+    public String choix1;
+    public String choix2;
+    public String titre;
+    public String texte;
+
+
+	public Evenement(float proba,String conditions) //Méthode pour créer un événement
+    {
+        id = nbEvenement;
+        nbEvenement++;
+        this.proba = proba;
+        this.conditions = conditions;
+    }
+
+    internal object getId()
+    {
+        return id;
+    }
+}
---
+++ Assets/Scripts/Events/GenerationEvenement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Bonjour, ce script va générer un événement
+// Ce n'est pas lui qui décide quel événement aura été choisi
+
+public class GenerationEvenement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
